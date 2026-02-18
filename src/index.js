c129e38b5a27b6f758e797d0ae542844852cf586
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-18 15:00:09 +0530

    feat : product context add

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import productReducer from './store/reducers/products';
-
+import {ProductsProvider} from './context/product-context'
 const rootReducer = combineReducers({
   shop: productReducer,
 });
@@ -16,9 +16,9 @@ const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <ProductsProvider  >
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
+  </ProductsProvider>
 );
